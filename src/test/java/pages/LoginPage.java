1c9ade5c0697e14e2217ec01e6e199f1e817159b
Lokesh Dhanumuri <lokesh@leadwinner.com>
2025-09-29 18:52:10 +0530

    Your commit message here

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -25,6 +25,7 @@ public class LoginPage {
 			File file = new File("src/test/resources/properties/login.properties");
 		    FileInputStream fis = new FileInputStream(file);
 			property.load(fis);
+			System.out.println("properties loaded");
 			fis.close();
 		}catch(Exception e) { 
 			e.printStackTrace();
@@ -35,25 +36,30 @@ public class LoginPage {
 	public void goToUrl() {
 		String url =  property.getProperty("url");
 		loginpage.navigate(url);
+		System.out.println("url passed");
 	}
 	public void enterUsername(String username) {
 		String usernamepath = property.getProperty("username");
 		loginpage.locator(usernamepath).pressSequentially(username);	
+		System.out.println("enter username");
 	}
 	
 	public void enterPassword(String password) {
 		String passwordpath =property.getProperty("password");
 		loginpage.locator(passwordpath).pressSequentially(password);
+		System.out.println("clicked enter password");
 	}
 	
 	public void clickLoginButton() {
 		String loginbutton = property.getProperty("loginbutton");
 		loginpage.locator(loginbutton).click();
+		System.out.println("clicked login button ");
 	}
 	
 	public void clickForgotPassword() {
 		String forgotpassword = property.getProperty("forgotPassword");
 		loginpage.locator(forgotpassword).click();
+		System.out.println("clicked forgot password");
 	}
 	
 	
--- src/test/java/utilities/Hooks.java
+++ src/test/java/utilities/Hooks.java
@@ -3,17 +3,16 @@ package utilities;
 import io.cucumber.java.*;
 
 public class Hooks {
-	
-	@Before
-	public static void setup() {
-		System.out.println("Before all");
-		
-	}
-	
-	@After
-	public static void teardown() {
-		System.out.println("After all");
-		
-	}
 
+    @BeforeAll
+    public static void setup() {
+        System.out.println("Before all scenarios");
+        PlaywrightFactory.init();  // single browser for all scenarios
+    }
+
+    @AfterAll
+    public static void teardown() {
+        System.out.println("After all scenarios");
+        PlaywrightFactory.tearDown();
+    }
 }
